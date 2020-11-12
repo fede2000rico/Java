0ba2360452d35c07e0d78fffa463076b8f6e7950
federicopiol <fede2000rico@yahoo.it>
2020-11-12 15:20:29 +0100

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld;
+//First Java App: It prints Helloworld!
+//@author fede2
+
+
+//Class declaration: public means that it's accessible from outside
+public class HelloWorld {
+    
+    //main function declaration: static means that it's the same for every object in the code
+    public static void main(String[] args) {
+        //Printout defoult function
+        System.out.println("HelloWorld!");
+    }
+    
+}
