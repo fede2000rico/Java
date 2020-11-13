4a4ea33862c17008b325c36046c14d4773cabcc3
federicopiol <fede2000rico@users.noreply.github.com>
2020-11-13 11:30:29 +0100

    First program

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,19 @@
+//First Java App: It prints Helloworld!
+//@author fede2
+
+
+//Class declaration: public means that it's accessible from outside
+public class HelloWorld {
+
+    //main function declaration: static means that it's the same for every object in the code
+    public static void hello() {
+        //Printout defoult function
+        System.out.println("ABX");
+    }
+
+    public static void main(String[] args) {
+        hello();
+    }
+
+}
+
