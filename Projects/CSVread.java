544a0de6484cea5de3aee58c589b83336f42c151
federicopiol <fede2000rico@users.noreply.github.com>
2020-11-15 17:58:36 +0100

    Issue #4 done

--- Projects/CSVread.java
+++ Projects/CSVread.java
@@ -6,32 +6,46 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class CSVread {
-    private String path ="";
+    //Create string's array that contains data
     private String[] region;
 
+    //Custom constructor
     public CSVread(String path, String dataregion){
-        this.path=path;
+        //Create variable to read the *.csv file
         String line = "";
+
+        //Split rule
         String splitBy = ",";
+
+        //Return array in case of error
         String[] err ={"Error"};
         try {
             // parsing a CSV file into BufferedReader class constructor
             try(BufferedReader br = new BufferedReader(new FileReader(path))){
+
+                //While there are data
                 while ((line = br.readLine()) != null) 
                 {
+                    //Split the line into an array and set into region
                     region = line.split(splitBy); 
+
+                    //Is this region?
                     if(region[3].contains(dataregion)){
                         region= Arrays.copyOfRange(region, 0, (region.length-1));
                         return;
                     }
                 }
             }
+
+        //Manage exception
         } catch (IOException e) {
             e.printStackTrace();
         }
         region= err;
     }
 
+
+    //Print entire value of region (array)
     public void print(){
         String full="";
         for (String str : region) {
@@ -40,11 +54,11 @@ public class CSVread {
         System.out.println(full);
     }
 
+    //Print only useful data (Italian version)
     public void printuseful(){
         System.out.println("Dati Bolzano");
         System.out.println("Incremento positivi: "+ region[12]);
         System.out.println("Incremento deceduti: " + region[14]);
-
     }
     
 }
