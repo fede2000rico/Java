d4aa21298850e987fb20fabca8aa13a7e96cce66
federicopiol <fede2000rico@users.noreply.github.com>
2020-11-15 17:30:35 +0100

    mainclass edits

--- Projects/mainclass.java
+++ Projects/mainclass.java
@@ -1,6 +1,8 @@
 package Projects;
 
 public class mainclass{
+    public static String fileURL = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-latest.csv";
+    public static String filePATH = "Projects/data.csv";
     public static void main(String[] args) {
         CSVread data = new CSVread();
         data.print(data.dataregion());
