3ca85613d84a29daf8f37b1d8356ed7a8c56c8d6
federicopiol <fede2000rico@users.noreply.github.com>
2020-11-15 11:03:45 +0100

    edits

--- Projects/fileURLdownload.java
+++ Projects/fileURLdownload.java
@@ -2,9 +2,7 @@ package Projects;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
